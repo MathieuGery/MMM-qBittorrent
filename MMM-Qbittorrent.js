d4e24e6b55a2b911d2a85255ca39c5eaa127207d
mathieugery <mathieu.gery@epitech.eu>
2021-03-11 21:46:08 +0100

    Add simple table with fake data

--- MMM-Qbittorrent.js
+++ MMM-Qbittorrent.js
@@ -7,48 +7,73 @@
 
 
 Module.register('MMM-qBittorrent', {
-  defaults: {
-    showUploadSpeed: true,
-    showDownloadSpeed: true,
-    showRatio: true,
-    showLeftSpace: true,
-    fetchInterval: 1 * 60 * 1000 //fetch interval default every minutes
-  },
-
-  getStyles: function() {
-		return [ 'modules/MMM-qBittorrent/MMM-qBittorrent.css', 'font-awesome.css'];
+	defaults: {
+		showUploadSpeed: true,
+		showDownloadSpeed: true,
+		showRatio: true,
+		showLeftSpace: true,
+		apiToken: "maeokafoqoisjeifjqiosjeif",
+		qbUrl: "localhost",
+		fetchInterval: 1 * 60 * 1000 //fetch interval default every minutes
 	},
 
-  getTranslations: function () {
+	getStyles: function () {
+		return ['modules/MMM-qBittorrent/MMM-qBittorrent.css', 'font-awesome.css'];
+	},
+
+	getTranslations: function () {
 		return {
 			en: 'translations/en.json',
 			sv: 'translations/fr.json'
 		}
 	},
 
-  getDom: function() {
-    let wrapper = document.createElement('table');
+	getDom: function () {
+		let wrapper = document.createElement('table');
 
-    wrapper.className = 'bright xsmall';
+		if (null == this.stats) {
+			wrapper.innerHTML = this.translate('LOADING');
+			wrapper.className = 'loading dimmed xsmall';
+			return wrapper;
+		}
+		wrapper.className = 'bright xsmall';
 
-    let headerRow = document.createElement('tr');
+		let headerRow = document.createElement('tr');
 		headerRow.className = 'normal header-row';
 		this.createTableCell(headerRow, this.translate('RATIO'), this.config.showRatio, 'ratio-header', 'center');
 		this.createTableCell(headerRow, this.translate('DOWNLOAD_SPEED'), this.config.showDownloadSpeed, 'download-speed-header', 'center');
 		this.createTableCell(headerRow, this.translate('UPLOAD_SPEED'), this.config.showUploadSpeed, 'upload-speed-header', 'center');
 		this.createTableCell(headerRow, this.translate('LEFT_SPACE'), this.config.showLeftSpace, 'left-space-header', 'center');
 
-    wrapper.appendChild(headerRow);
-    return wrapper
-  },
+		wrapper.appendChild(headerRow);
 
-  // Creates a table row cell.
+		for (let i = 0; i < 1; ++i) {
+			let row = document.createElement('tr');
+			row.className = 'normal bright stats-row';
+
+			const stat = this.stats;
+			this.createNumberTableCell(row, stat.ratio, this.config.showRatio, 'ratio');
+			this.createTableCell(row, stat.downloadSpeed, this.config.showDownloadSpeed, 'downloadSpeed');
+			this.createNumberTableCell(row, stat.uploadSpeed, this.config.showUploadSpeed, 'uploadSpeed');
+			this.createNumberTableCell(row, stat.leftSpace, this.config.showLeftSpace, 'uploadSpeed');
+			wrapper.appendChild(row);
+		}
+		return wrapper
+	},
+
+	createNumberTableCell: function (row, number, show, className) {
+		if (!show)
+			return;
+
+		const text = new Intl.NumberFormat().format(number);
+		this.createTableCell(row, text, show, className);
+	},
+	// Creates a table row cell.
 	// @param row - The table row to add cell to.
 	// @param text - The text to show.
 	// @param show - Whether to actually show.
 	// @param align - Text align: 'left', 'center' or 'right'.
-	createTableCell: function(row, text, show, className, align = 'right')
-	{
+	createTableCell: function (row, text, show, className, align = 'right') {
 		if (!show)
 			return;
 
@@ -61,7 +86,32 @@ Module.register('MMM-qBittorrent', {
 		row.appendChild(cell);
 	},
 
-  start: function () {},
-  notificationReceived: function() {},
-  socketNotificationReceived: function() {},
+	start: function () {
+		this.stats = null;
+
+		// Tell node_helper to load stats at startup.
+		this.sendSocketNotification('GET_STATS', {
+			identifier: this.identifier,
+			apiToken: this.config.apiToken,
+			qbUrl: this.config.qbUrl
+		});
+		let interval = Math.max(this.config.fetchInterval, 1000);
+		let self = this;
+		setInterval(function () {
+			self.sendSocketNotification('GET_STATS', {
+				identifier: this.identifier,
+				apiToken: this.config.apiToken,
+				qbUrl: this.config.qbUrl
+			});
+		}, interval); // In millisecs.
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === 'STATS_RESULT') {
+			console.error(this.name + ' error when fetching data: ' + "la bite");
+			this.stats = payload.stats;
+			this.updateDom(0);
+		}
+	},
+	notificationReceived: function () { },
 })
--- node_helper.js
+++ node_helper.js
@@ -12,6 +12,17 @@ module.exports = NodeHelper.create({
 		//console.log('Starting node_helper for: ' + this.name);
 	},
 
+	getStats: function(payload) {
+		let identifier = payload.identifier;
+		const stats = { ratio: 10,
+			downloadSpeed: 100,
+			uploadSpeed: 200,
+			leftSpace: 50
+		};
+		console.error(this.name + ' error when fetching data: ' + stats.ratio);
+		this.sendSocketNotification('STATS_RESULT', {identifier: identifier, stats: stats} );
+	},
+
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === 'GET_STATS') {
 			this.getStats(payload);
